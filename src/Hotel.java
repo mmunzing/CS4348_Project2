81aba79648143c2a971d7dd5e665c4ea04c2588f
mmunzing <michael.munzing@att.net>
2015-10-18 11:52:37 -0500

    Updated with more semaphores!

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,16 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 //https://orajavasolutions.wordpress.com/tag/sleeping-barber-problem/
 
 public class Hotel {
 	protected static int roomNum = 1;
-	protected static Semaphore s1 = new Semaphore(-1, true);
-	protected static Semaphore s2 = new Semaphore(2, true);
-	protected static Semaphore s3 = new Semaphore(-1, true);
+	protected static Semaphore s1 = new Semaphore(0, true);
+	protected static Semaphore s2 = new Semaphore(0, true);
+	protected static Semaphore checkinQSema = new Semaphore(1, true);
+	protected static Semaphore bellhopQSema = new Semaphore(1, true);
 	
-	public static void main(String[] args) {
-		System.out.println("Simulation starts");
-		
+	protected static Queue<Guest> checkinQ = new LinkedList<Guest>();
+	protected static Queue<Guest> bellhopQ = new LinkedList<Guest>();
+	
+	private void runHotel(){
 		Employee empArr[] = new Employee[2];
 		Thread empThreads[] = new Thread[2];
 		Bellhop bellArr[] = new Bellhop[2];
@@ -19,27 +23,35 @@ public class Hotel {
 	    Thread guestThreads[] = new Thread[25];
 	    
 	    // Create 2 front desk workers
-	    for(int i = 0; i < 2; i++){
+	    for(int i = 0; i < empArr.length; i++){
 	    	empArr[i] = new Employee(i);
 	    	empThreads[i] = new Thread(empArr[i]);
 	    	empThreads[i].start();
 	    }
 	    
 	    // Create 2 Bellhops
-	    for(int j = 0; j < 2; j++){
+	    for(int j = 0; j < bellArr.length; j++){
 	    	bellArr[j] = new Bellhop(j);
 	    	bellThreads[j] = new Thread(bellArr[j]);
 	    	bellThreads[j].start();
 	    }
 	    
 	    // Create 25 Guests
-	    for(int k = 0; k < 25; k++ ) 
-	      {
-	    	 guestArr[k] = new Guest(k);
-	         guestThreads[k] = new Thread( guestArr[k] );
-	         guestThreads[k].start();
-	      }
-
+	    for(int k = 0; k < guestArr.length; k++ ) {
+	    	guestArr[k] = new Guest(k);
+	        guestThreads[k] = new Thread( guestArr[k] );
+	        guestThreads[k].start();
+	    }
+		
+	}	// End of runHotel function
+	
+	public static void main(String[] args) {
+		Hotel hotel = new Hotel();
+		
+		System.out.println("Simulation starts");
+		
+		hotel.runHotel();
+		
 	    try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -54,6 +66,7 @@ public class Hotel {
 
 	class Employee implements Runnable {
 	private int empNum;
+	private Guest helping;
 	Employee(int num) {
 		this.empNum = num;
 	}	// End of constructor
@@ -69,11 +82,15 @@ public class Hotel {
 		try
 		{
 			Hotel.s2.acquire();
+			Hotel.checkinQSema.acquire();
+			helping = Hotel.checkinQ.remove();
+			Hotel.checkinQSema.release();
 		}
 		catch (InterruptedException e)
 		{
 		}
-		System.out.println("Front desk employee" + empNum + " registers guest " + 1 + " and assigns room " + Hotel.roomNum);
+		System.out.println("Front desk employee " + empNum + " registers guest " + helping.getNum() + " and assigns room " + Hotel.roomNum);
+		helping.setRoomNum(Hotel.roomNum);
 		
 		Hotel.roomNum++;
 	}	// End of run function
@@ -107,7 +124,6 @@ class Guest implements Runnable {
 	private int guestNum;
 	private int numBags;
 	private int roomNum;
-	//protected Semaphore waitForEmployee = new Semaphore(2, true );
 	
 	Guest(int num){
 		this.guestNum = num;
@@ -125,39 +141,44 @@ class Guest implements Runnable {
 	   try
 	   {
 	      Hotel.s1.acquire();
+	      
+	      Hotel.checkinQSema.acquire();
+	      Hotel.checkinQ.add(this);
+	      Hotel.checkinQSema.release();
+	      
+	      randBags();
+		   if(numBags == 1)
+			   System.out.println( "Guest " + guestNum + " enters hotel with " + numBags + " bag" );
+		   else
+			   System.out.println( "Guest " + guestNum + " enters hotel with " + numBags + " bags" );
+		   Thread.sleep(1000);
 	   }
 	   catch (InterruptedException e)
 	   {
-	   }
+	   }	   
 	   
-	   randBags();
-	   if(numBags == 1)
-		   System.out.println( "Guest " + guestNum + " enters hotel with " + numBags + " bag" );
-	   else
-		   System.out.println( "Guest " + guestNum + " enters hotel with " + numBags + " bags" );
-	   try {
-		   Thread.sleep(1000);
-	   } 
-	   catch (InterruptedException e) {
-	   }
-	   
-	}
-	
-	public void post() {
 	   Hotel.s1.release();
-	}
+	}	// End of run function
 	
-	private int getNum(){
+//	public void post() {
+//	   Hotel.s1.release();
+//	}	// End of post function
+	
+	public int getNum(){
 		return guestNum;
-	}
+	}	// End of getNum function
 	
-	private void setRoomNum(int num){
+	public void setRoomNum(int num){
 		this.roomNum = num;
-	}
+	}	// End of setRoomNum function
+	
+	public int getRoomNum(){
+		return this.roomNum;
+	}	// End of getRoomNum function
 	
 	private void randBags(){
 		Random randomNum = new Random();
     	this.numBags = randomNum.nextInt(6);
-	}
-}
+	}	// End of randBags function
+}	// End of Employee Class
 
